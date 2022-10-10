980548d2d2cd949ef142ce83d48625e504ee58bd
Khalidprithy <khalidprithy@gmail.com>
2022-10-10 23:23:18 +0600

    footer changed

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='pt-12'>All rights reserved by &copy; Kbin</p>
+            <p className='pt-12 text-3xl'>All rights reserved by &copy; Kbin</p>
         </div>
     );
 };
