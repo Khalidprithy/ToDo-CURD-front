26758018407173458da93b0a018a5c5a79f9bef8
Khalidprithy <khalidprithy@gmail.com>
2023-01-21 12:59:02 +0600

    Design Update

--- src/components/Pages/TodoInput.js
+++ src/components/Pages/TodoInput.js
@@ -28,13 +28,13 @@ const TodoInput = ({ refetch }) => {
     }
     return (
         <div className=''>
-            <h1 className='text-xl md:text-3xl font-bold m-6 font-mono'>Add your Tasks</h1>
+            <h1 className='text-xl md:text-3xl font-bold m-1 md:m-6 font-mono'>Add your Tasks</h1>
             <form
                 onSubmit={handleTask}
             >
                 <div className='flex items-center justify-center gap-2 w-full'>
                     <input className='border rounded-md px-4 py-1 focus:outline-red-500 w-2/3 md:w-6/12 h-12' type="text" name="task" id="task" placeholder='Write your task here' maxlength="100" required />
-                    <input className='btn btn-md bg-teal-500 border-0' type="submit" value="Add Task" />
+                    <input className='btn btn-md bg-teal-500 border-0 text-gray-50' type="submit" value="Add Task" />
                 </div>
             </form>
         </div>
--- src/components/Pages/TodoOutput.js
+++ src/components/Pages/TodoOutput.js
@@ -39,77 +39,60 @@ const TodoOutput = ({ todoData, isLoading, refetch }) => {
     });
 
     return (
-        <div className='mt-5'>
-            <div>
-                <div className="w-full mt-6">
-                    <div className='flex items-center justify-center'>
-                        <ul
-                            className="flex gap-2 items-center justify-center mb-0 list-none pt-3 pb-4 mx-6"
-                            role="tablist"
-                        >
-                            <li className="flex-auto text-center w-full">
-                                <a
-                                    className={
-                                        "text-base font-semibold capitalize px-5 py-1 md:py-3 shadow rounded-md block leading-normal " +
-                                        (openTab === 1
-                                            ? "text-teal-400 bg-white border-b-2 border-red-400 shadow-md"
-                                            : "text-gray-800 bg-white")
-                                    }
-                                    onClick={e => {
-                                        e.preventDefault();
-                                        setOpenTab(1);
-                                    }}
-                                    data-toggle="tab"
-                                    href="#link1"
-                                    role="tablist"
-                                >
-                                    All Task
-                                </a>
-                            </li>
-                            <li className="flex-auto text-center w-full">
-                                <a
-                                    className={
-                                        "text-base font-semibold capitalize px-5 py-1 md:py-3 shadow rounded-md block leading-normal " +
-                                        (openTab === 2
-                                            ? "text-teal-400 bg-white border-b-2 border-red-400 shadow-md"
-                                            : "text-gray-800 bg-white")
-                                    }
-                                    onClick={e => {
-                                        e.preventDefault();
-                                        setOpenTab(2);
-                                    }}
-                                    data-toggle="tab"
-                                    href="#link2"
-                                    role="tablist"
-                                >
-                                    Completed
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="relative flex flex-col min-w-0 break-words w-full mb-6">
-                        <div className="px-1 md:px-4 py-5 flex-auto">
-                            <div className="tab-content tab-space">
-                                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-                                    <motion.ul initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
-                                        {
-                                            sortedData?.map(task => <Task
-                                                key={task._id}
-                                                task={task}
-                                                refetch={refetch}
-                                                isLoading={isLoading}
-                                                handleDelete={handleDelete}
-                                            ></Task>)
-                                        }
-                                    </motion.ul>
-                                </div>
-                                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+        <div className='mt-2 md:mt-5'>
+            <div className="w-full mt-1 md:mt-6">
+                <div className='flex items-center justify-center'>
+                    <ul
+                        className="flex gap-2 items-center justify-center mb-0 list-none pt-3 pb-4 mx-6"
+                        role="tablist"
+                    >
+                        <li className="flex-auto text-center w-full">
+                            <a
+                                className={
+                                    "text-base font-semibold capitalize px-5 py-1 md:py-3 shadow rounded-md block leading-normal " +
+                                    (openTab === 1
+                                        ? "text-teal-400 bg-white border-b-2 border-red-400 shadow-md"
+                                        : "text-gray-800 bg-white")
+                                }
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setOpenTab(1);
+                                }}
+                                data-toggle="tab"
+                                href="#link1"
+                                role="tablist"
+                            >
+                                All Task
+                            </a>
+                        </li>
+                        <li className="flex-auto text-center w-full">
+                            <a
+                                className={
+                                    "text-base font-semibold capitalize px-5 py-1 md:py-3 shadow rounded-md block leading-normal " +
+                                    (openTab === 2
+                                        ? "text-teal-400 bg-white border-b-2 border-red-400 shadow-md"
+                                        : "text-gray-800 bg-white")
+                                }
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setOpenTab(2);
+                                }}
+                                data-toggle="tab"
+                                href="#link2"
+                                role="tablist"
+                            >
+                                Completed
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6">
+                    <div className="px-1 md:px-4 py-5 flex-auto">
+                        <div className="tab-content tab-space">
+                            <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                                <motion.ul initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }}>
                                     {
-                                        todoData?.filter(value => {
-                                            if (value.status === true) {
-                                                return value;
-                                            }
-                                        }).map(task => <Task
+                                        sortedData?.map(task => <Task
                                             key={task._id}
                                             task={task}
                                             refetch={refetch}
@@ -117,14 +100,28 @@ const TodoOutput = ({ todoData, isLoading, refetch }) => {
                                             handleDelete={handleDelete}
                                         ></Task>)
                                     }
-                                </div>
+                                </motion.ul>
+                            </div>
+                            <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                                {
+                                    todoData?.filter(value => {
+                                        if (value.status === true) {
+                                            return value;
+                                        }
+                                    }).map(task => <Task
+                                        key={task._id}
+                                        task={task}
+                                        refetch={refetch}
+                                        isLoading={isLoading}
+                                        handleDelete={handleDelete}
+                                    ></Task>)
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div>
-
                 <div className='flex justify-center gap-2 py-2'>
                     {
                         todoData?.length === 0 ? ''
