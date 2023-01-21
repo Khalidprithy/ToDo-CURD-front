f4e6ba192630305391adf106066a947dc40da7f7
Khalidprithy <khalidprithy@gmail.com>
2023-01-21 12:40:12 +0600

    small changes

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='mb-4'>
-            <p className='pt-12 text-2xl'>All rights reserved by &copy; <a className='text-error' target="_blank" rel="noreferrer" href="https://devkbin.netlify.app/">Dev.Kbin</a> </p>
+            <p className='pt-12 text-xl md:text-2xl'>All rights reserved by &copy; <a className='text-error' target="_blank" rel="noreferrer" href="https://devkbin.netlify.app/">Dev.Kbin</a> </p>
         </div>
     );
 };
--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -8,14 +8,13 @@ const Header = () => {
     // console.log(open)
 
     return (
-        <div className='mx-4'>
-            <div
-                onMouseEnter={() => setOpen(true)}
-                onMouseLeave={() => setOpen(false)}
-                className="flex items-center justify-between bg-teal-500 m-3 mx-auto w-11/12 md:w-8/12 ease-in-out duration-500 rounded-md text-white h-20">
-                <div className="text-white flex items-center justify-start">
+        <div
+            onMouseEnter={() => setOpen(true)}
+            onMouseLeave={() => setOpen(false)}
+            className="flex items-center justify-between bg-teal-500 dark:bg-teal-600 m-3 mx-auto w-11/12 md:w-8/12 ease-in-out duration-500 rounded-md text-white h-16">
+            <div className="text-white flex items-center justify-start">
 
-                    {/* <div className="dropdown">
+                {/* <div className="dropdown">
                         <label tabIndex="0" className="btn btn-ghost rounded-full m-1 md:hidden lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
@@ -26,12 +25,12 @@ const Header = () => {
                         </ul>
 
                     </div> */}
-                    <div className='flex items-center gap-2 ml-4 md:ml-10'>
-                        <BsCheck2Square className='text-2xl' />
-                        <h4 className='text-xl md:text-3xl'>Todo</h4>
-                    </div>
+                <div className='flex items-center gap-2 ml-4 md:ml-10'>
+                    <BsCheck2Square className='text-2xl' />
+                    <h4 className='text-xl md:text-3xl'>Todo</h4>
                 </div>
-                {/* <div className={` ${open ? "hidden md:block ease-in-out duration-500" : "hidden"} `}>
+            </div>
+            {/* <div className={` ${open ? "hidden md:block ease-in-out duration-500" : "hidden"} `}>
                     <ul className="menu menu-horizontal p-0">
                         <ul className="menu menu-horizontal p-0">
                             <li className='text-2xl mr-2 hover:text-gray-500'>Completed</li>
@@ -41,19 +40,19 @@ const Header = () => {
                     </ul>
                 </div> */}
 
-                <div className="flex">
-                    <button className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                    </button>
-                    <button className="btn btn-ghost btn-circle mx-1 mr-8">
-                        <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                            <span className="badge badge-xs badge-warning indicator-item"></span>
-                        </div>
-                    </button>
-                </div>
+            <div className="flex">
+                <button className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
+                <button className="btn btn-ghost btn-circle mx-1 mr-8">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                        <span className="badge badge-xs badge-warning indicator-item"></span>
+                    </div>
+                </button>
             </div>
         </div>
+
     );
 };
 
